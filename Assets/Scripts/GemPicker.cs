e95cef0340737a6b54dd58411b95219291251287
Ivz <113674087+okuenz@users.noreply.github.com>
2024-03-04 22:17:33 +0300

    first

--- Assets/Scripts/GemPicker.cs
+++ Assets/Scripts/GemPicker.cs
@@ -5,15 +5,7 @@ using TMPro;
 public class GemPicker : MonoBehaviour
 {
     public TMP_Text coinsText;
-    public AudioSource coinSound; // Ссылка на источник звука
-    public AudioClip coinPickupSound; // Звук сбора монетки
     private float coins = 0;
-
-    private void Start()
-    {
-        coinSound = GetComponent<AudioSource>(); // Получаем доступ к Audio Source
-        coinSound.clip = coinPickupSound; // Назначаем звук сбора монетки
-    }
 
     private void OnTriggerEnter2D(Collider2D coll)
     {
@@ -21,7 +13,6 @@ public class GemPicker : MonoBehaviour
         {
             coins++;
             coinsText.text = coins.ToString();
-            SoundManager.instance.PlayCoinPickupSound(); // Вызываем метод воспроизведения звука из SoundManager
             Destroy(coll.gameObject);
         }
     }
